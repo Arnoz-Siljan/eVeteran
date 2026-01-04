81fa8223506408d7a36fbca073631e30442ab9f8
Arnoz-Siljan <arnoz.siljan@student.um.si>
2026-01-04 16:48:33 +0100

    fix paths for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import { dirname, resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,20 +8,14 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-  // Nastavite 'base' na ime vašega GitHub repozitorija
-  // Če je URL: https://username.github.io/repo-name/, potem je base: '/repo-name/'
-  // Če je URL: https://username.github.io/, potem je base: '/'
   base: '/eVeteran/',
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
-      '@': dirname(__dirname) + '/src',
+      '@': resolve(__dirname, './src'),
     },
   },
 })
